420544708a581aea3ce3dfefac27b73de02954c5
Marthijn Verhoeven <mmamhver@student.avans.nl>
2016-06-10 15:58:40 +0200

    Tile and Game algorithms
    Tile checks,
    Game checks,
    Tests for Tile and Game.

--- app/models/Tile.ts
+++ app/models/Tile.ts
@@ -12,38 +12,66 @@ namespace Application.Model
 			matchesWholeSuit: boolean,
 			__v: number
 		};
+		match: {
+      		foundBy: string,
+      		otherTileId: string,
+      		foundOn: string
+    	}
 		_id: string;
 		
 		/**
-		 * Checks if given tile is on blocking on top of current instance.
+		 * Returns true if the tile is being blocked on the top by the given tile.
 		 */
-		private isOnTop(tile : Tile) : boolean
+		public isTileBlockedOnTopBy(tile : Tile) : boolean
 		{
 			var self = this;
+
+			//The given tile is in the same spot as this tile, therefore it is not being blocked
+			if(self.xPos === tile.xPos && self.yPos === tile.yPos && self.zPos === tile.zPos) {
+				return false;
+			}
+
+			/* Just keep for when needed!
+			console.log("ME    TILE x:" + self.xPos + " y:" + self.yPos + " z:" + self.yPos);
+			console.log("OTHER TILE x:" + tile.xPos + " y:" + tile.yPos + " z:" + tile.yPos);
+
+			console.log("Selected tile conflicting xpos: " + (self.xPos -1 == tile.xPos || self.xPos == tile.xPos || self.xPos + 1 == tile.xPos));
+			console.log("Selected tile conflicting ypos: " + (self.yPos -1 == tile.yPos || self.yPos == tile.yPos || self.yPos + 1 == tile.yPos));
+			console.log("Selected tile conflicting zpos: " + (self.zPos +1 == tile.zPos));
+			*/
+
 			return ((self.xPos -1 === tile.xPos || self.xPos === tile.xPos || self.xPos + 1 === tile.xPos)
-				&& (self.yPos -1 === tile.yPos || self.yPos === tile.yPos || self.yPos + 1 === tile.yPos));
+				&& (self.yPos -1 === tile.yPos || self.yPos === tile.yPos || self.yPos + 1 === tile.yPos)
+				&& (self.zPos +1 === tile.zPos));
 		}
 		
 		/**
-		 * Checks if given tile is blocking to the left or the right of current instance.
+		 * Returns true if the tile is being blocked on the side by the given tile.
 		 */
-		private isLeftOrRight(tile: Tile) : boolean
+		public isTileBlockedOnTheSideBy(tile: Tile) : boolean
 		{
 			var self = this;
+
+			//The given tile is in the same spot as this tile, therefore it is not being blocked
+			if(self.xPos === tile.xPos && self.yPos === tile.yPos && self.zPos === tile.zPos) {
+				return false;
+			}
+
 			return ((self.xPos - 2 === tile.xPos || self.xPos + 2 === tile.xPos)
-				&& (self.yPos - 1 === tile.yPos || self.yPos === tile.yPos || self.yPos + 1 === tile.yPos))
+				&& (self.yPos - 1 === tile.yPos || self.yPos === tile.yPos || self.yPos + 1 === tile.yPos)
+				&& (self.zPos === tile.zPos));
 		}
 		
-		public canMatch(tiles : Tile[]) : boolean
+		public isTileBlockedBy(tiles : Tile[]) : boolean
 		{
 			var self = this;
 			for(var tile of tiles)
 			{
-				if(!self.isOnTop(tile) && this.isLeftOrRight(tile)) {
-					
+				if(self.isTileBlockedOnTopBy(tile) || this.isTileBlockedOnTheSideBy(tile)) {
+					return true;
 				}
 			}
-			return null;
+			return false;
 		}
 	}
 }
